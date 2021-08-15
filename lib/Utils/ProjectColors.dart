5ca5d23a55993a0bc1f8ec3f9959601918d6987b
Rômullo Cordeiro Rodrigues <45169227+xXDeonoXx@users.noreply.github.com>
2021-08-15 19:33:48 -0300

    feat: add pattens blue color

--- lib/Utils/ProjectColors.dart
+++ lib/Utils/ProjectColors.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 
-  lightBlue() {
-    return Color(0xFF0058ff);
-  }
+lightBlue() {
+  return Color(0xFF0058ff);
+}
 
-  teal() {
-    return Color(0xff1b2c48);
-  }
+teal() {
+  return Color(0xff1b2c48);
+}
 
-  darkerBlue() {
-    return Color(0xff0e1834);
-  }
+darkerBlue() {
+  return Color(0xff0e1834);
+}
 
-  indigo() {
-    return Colors.indigo[900];
-  }
+indigo() {
+  return Colors.indigo[900];
+}
+
+pattensBlue() {
+  return Color(0xffCCE4FB);
+}
